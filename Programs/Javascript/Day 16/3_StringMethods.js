4fd1def4ac2fcdbba5280d5b931ead48a88dfe25
ShadabHasanHakimi <shadabhasan786123@gmail.com>
2024-02-29 08:38:36 +0530

    Commited

---
+++ Programs/Javascript/Day 16/3_StringMethods.js
@@ -0,0 +1,9 @@
+// str.trim() : Removes whitespaces from both the ends of string & returns a new one
+// does not remove whitespaces from between the string
+// It does not changes the original string, instead it creates a new string without spaces
+let str1 = "   Hello  Everyone     ";
+console.log(str1);
+
+console.log(str1.trim());
+
+// Note: String are Immutable in JS
