6a995e1134ba4df5d0087c999fe3e5a02cbf39db
ShadabHasanHakimi <shadabhasan786123@gmail.com>
2024-04-22 08:12:55 +0530

    Commited

---
+++ Programs/Javascript/Day 25/35_API.js
@@ -0,0 +1,13 @@
+// API: Application Programming Interface
+
+// using random APIs 
+
+// For testing API requests we can use:
+// Hoppscotch
+// Postman
+
+// Status code 
+// 200 - OK
+// 404 - not found
+// 400 - bad request
+// 500 - internal server error
---
+++ Programs/Javascript/Day 25/36_Json.js
@@ -0,0 +1,4 @@
+let jsonRes = '{ "fact": "Normal body temperature for a cat is 102 degrees F.", "length": 51 }';
+
+let validJsonRes = JSON.parse(jsonRes);
+console.log(validJsonRes);
