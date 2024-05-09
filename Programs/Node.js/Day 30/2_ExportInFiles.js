645d0c99236e35b4b410a7633dcf01eb62b28dc7
Shadab Hasan Hakimi <shadabhasanhakimi@gmail.com>
2024-05-09 08:29:32 +0530

    Node.js completed

--- Programs/Node.js/Day 30/2_ExportInFiles.js
+++ Programs/Node.js/Day 30/2_ExportInFiles.js
@@ -1,16 +1,16 @@
-const sum = (a, b) => a+b;
-const mul = (a, b) => a*b;
-const g = 9.8;
-const Pi = 3.14;
+export const sum = (a, b) => a+b;
+export const mul = (a, b) => a*b;
+export const g = 9.8;
+export const Pi = 3.14;
 
 // module.exports: a special object that include data to be exported to other files
 // module.exports = 123;
 
-let obj = {
-    sum: sum,
-    mul: mul,
-    g: g,
-    Pi: Pi
-};
+// export let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     Pi: Pi
+// };
 
-module.exports = obj;
+// module.exports = obj;
--- Programs/Node.js/Day 30/2_ImportFiles.js
+++ Programs/Node.js/Day 30/2_ImportFiles.js
@@ -1,6 +1,19 @@
 // require() : a built-in fucntion to include external modules that exist in separate files.
-const importedValue = require("./2_ExportInFiles"); // values imported from 2_ExportInFiles
+// const importedValue = require("./2_ExportInFiles"); // values imported from 2_ExportInFiles
 
-console.log(importedValue);
-console.log(importedValue.sum(2,2));
-console.log(importedValue.mul(3, 5));
+// In ES6 version of JS, we can use import instead of require
+// For using impory we have to use npm init and make a package.json file
+// Then we will have to add a new key value pair in the package.json file
+// "type": "module"
+
+// We can also import selected values from exported values but in require all the data present in the file is imported
+// Loading is synchronous for 'require' but can be asynchronous for 'import'
+// We should use only one of the both import or require
+import{sum, Pi} from "./2_ExportInFiles.js";
+
+console.log(sum(2,2));
+// console.log(importedValue.mul(3, 5));
+
+// random-words package is installed through which we can import random words
+import{generate} from "random-words";
+console.log(generate());
