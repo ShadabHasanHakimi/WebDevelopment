04689d16c5120c9c1c9e09ed2df72ff3a906609d
Shadab Hasan Hakimi <shadabhasanhakimi@gmail.com>
2024-05-16 08:31:25 +0530

    Commited

--- Programs/Express.js/Day 32/3_InstagramEJS.js
+++ Programs/Express.js/Day 32/3_InstagramEJS.js
@@ -2,15 +2,28 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+
 const port = 8080;
+
+app.use(express.static("public"));
+// for using public outside the main folder
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/ig/:username", (req, res)=>{
+    // Let us suppose that this data is from database
+    const instaData = require("./data.json");
     let {username} = req.params;
-    console.log(`username: ${username}`);
-    res.render("3_InstagramEJS.ejs", {username: username});
+    let data = instaData[username];
+    console.log(data);
+    if(data){
+        res.render("3_InstagramEJS.ejs", {data});
+    }
+    else{
+        res.render("3_error");
+    }
 });
 
 app.listen(port, ()=>{
--- Programs/Express.js/Day 32/4_LoopsInEjs.js
+++ Programs/Express.js/Day 32/4_LoopsInEjs.js
@@ -8,5 +8,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res)=>{
-    app.render("4_LoopsInEjs.ejs");
+    res.render("4_LoopsInEjs.ejs");
 });
+
+app.listen(port, ()=>{
+    console.log(`Listening to the port ${port}`);
+})
