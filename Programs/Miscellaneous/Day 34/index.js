5e462df0bd2f80fc3081b55b136926ecac8ca51f
Shadab Hasan Hakimi <shadabhasanhakimi@gmail.com>
2024-05-29 08:26:14 +0530

    Commited

--- Programs/Miscellaneous/Day 34/index.js
+++ Programs/Miscellaneous/Day 34/index.js
@@ -7,12 +7,16 @@ const port = 8080;
 
 const path = require("path");
 
-const  bodyParser = require("body-parser");
-app.use(bodyParser.json());
 // using uuid package to create unique uuid
 const {v4: uuidv4} = require("uuid");
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+// requiring method-override
+const methodOverride = require("method-override");
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -65,16 +69,33 @@ app.post("/posts", (req, res) =>{
     res.redirect("/posts");
 });
 
+// For adding data to a existing post
+// Using Hoppscotch to send patch request
 app.patch("/posts/:id", (req, res) => {
     let {id} = req.params;
     let newContent = req.body.content;
-    console.log(id);
     console.log(newContent);
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts/");
 });
+
+// Edit route: Create form for update
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    console.log(id);
+    posts = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+// Destroy route: to delete a post
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    // posts.filter() will filter the posts whose id is !== given id
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("http://localhost:8080/posts/");
+})
 
 app.listen(port, ()=>{
     console.log(`listening to the port ${port}`);
