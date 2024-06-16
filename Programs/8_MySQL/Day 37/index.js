8cd6ff52c9255861f190ee1cb0a7920063f69523
Shadab Hasan Hakimi <shadabhasanhakimi@gmail.com>
2024-06-16 08:41:10 +0530

    Commited

--- Programs/8_MySQL/Day 37/index.js
+++ Programs/8_MySQL/Day 37/index.js
@@ -1,14 +1,47 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+// below we are creating connection to my sql workbench
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: '@shadab786'
+});
+
+// we can pass queries using a variable
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+// inserting data in bulk using faker
 
 // faker creates new data when called
-
 let getRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+  return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password()
+  ];
 };
 
-console.log(getRandomUser());
+let data = [];
+for(let i = 0; i<100; i++){
+  data.push(getRandomUser());
+}
+
+try{
+  connection.query(q, [data], (err, result)=>{
+    if(err) 
+      throw err;
+    // result is a array of tables
+    console.log(result);
+    // console.log(result.length);
+    // console.log(result[0]);
+    // console.log(result[1]);
+  });
+}
+catch(err){
+  console.log(err);
+}
+
+connection.end();
