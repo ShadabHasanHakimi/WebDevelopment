f3626fe75243aed955218d7770bd985001a3f416
Shadab Hasan Hakimi <shadabhasanhakimi@gmail.com>
2024-07-27 08:17:12 +0530

    MongoDB Completed

---
+++ Programs/9_MongoDB/Day 40/init.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const Chat = require("./models/chat")
+
+main()
+.then(() => {
+    console.log("Connection successful!");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats = [{
+    from: "shadab",
+    to: "adab",
+    msg: "Hello! How are you?",
+    created_at: new Date(),
+    updated_at:{type: Date, default: undefined}
+},
+{
+    from: "adab",
+    to: "shadab",
+    msg: "I am Fine! What about you?",
+    created_at: new Date(),
+    updated_at:{type: Date, default: undefined}
+},
+{
+    from: "rohit",
+    to: "amit",
+    msg: "Hello! Do you remember me?",
+    created_at: new Date(),
+    updated_at:{type: Date, default: undefined}
+},
+{
+    from: "priya",
+    to: "raghav",
+    msg: "Can you please help me in today's homework?",
+    created_at: new Date(),
+    updated_at:{type: Date, default: undefined}
+},
+{
+    from: "sumit",
+    to: "riya",
+    msg: "Can you please share today's classwork?",
+    created_at: new Date(),
+    updated_at:{type: Date, default: undefined}
+}];
+
+Chat.insertMany(allChats)
